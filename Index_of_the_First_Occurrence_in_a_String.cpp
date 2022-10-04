b4010f435cc4839006314fca992b5b3fbf0fae81
Angel-W10 <angelwadhawan10@gmail.com>
2022-10-04 15:40:17 +1030

    Index_of_the_First_Occurrence_in_a_String

--- Index_of_the_First_Occurrence_in_a_String.cpp
+++ Index_of_the_First_Occurrence_in_a_String.cpp
@@ -6,20 +6,35 @@
 using namespace std;
 
 int strStr(string haystack, string needle) {
-    int len_h = haystack.length();
-    int len_n = needle.length();
-    int counter = 0;
-    int out = 0;
 
-    while (counter != len_n){
-        if()
+    int counter = 0;
+    bool match = false;
+    int out = 0;
+    int h = 0;
+    int n = 0;
+
+    while (h<haystack.length() && n < needle.length()){
+        if(haystack[h]==needle[n]){
+            h++;
+            n++;
+        }
+        else{
+            h=h-n+1;
+            n=0;
+        }
     }
+    if (n=needle.length()){
+        return h-n;
+    }
+    return -1;
+
 }
 
 
 int main(){
-
-    strStr("haystack", "sta");
+    string haystack = "leetcode", needle = "leeto";
+    int out = strStr(haystack, needle);
+    cout<<"out = "<<out<<endl;
 
     return 0;
 }
