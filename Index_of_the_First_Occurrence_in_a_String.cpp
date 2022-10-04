e7cfef9c0923808148295576571426cb2528af02
Angel-W10 <angelwadhawan10@gmail.com>
2022-10-04 14:43:02 +1030

    lol

---
+++ Index_of_the_First_Occurrence_in_a_String.cpp
@@ -0,0 +1,25 @@
+// index of the first occurence in a string
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int strStr(string haystack, string needle) {
+    int len_h = haystack.length();
+    int len_n = needle.length();
+    int counter = 0;
+    int out = 0;
+
+    while (counter != len_n){
+        if()
+    }
+}
+
+
+int main(){
+
+    strStr("haystack", "sta");
+
+    return 0;
+}
