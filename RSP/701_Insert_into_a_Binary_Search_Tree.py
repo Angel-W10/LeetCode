12760f689151444c8ed44b672ce600f739c31ea2
Angel-W10 <angelwadhawan10@gmail.com>
2022-12-19 17:24:25 +1030

    701_Insert_into_a_Binary_Search_Tree

---
+++ RSP/701_Insert_into_a_Binary_Search_Tree.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def insertIntoBST(self, root, val):
+        if(root == None):
+            root = TreeNode(val)
+        else:
+            temp = root
+            if(val < temp.val):
+                if(temp.left == None):
+                    temp.left = TreeNode(val)
+                else:
+                    self.insertIntoBST(temp.left, val)
+            if(val > temp.val):
+                if(temp.right == None):
+                    temp.right = TreeNode(val)
+                else:
+                    self.insertIntoBST(temp.right, val)
+
+        return root
+
+    
